ffb48ec1707525bb45c9dca87c3a74dd8cc066f2
Jason19951003 <Jason19951003@gmail.com>
2024-05-14 10:36:40 +0800

    實現首頁和Error

--- SpringMVC/src/main/java/mvc/user/controller/UserController.java
+++ SpringMVC/src/main/java/mvc/user/controller/UserController.java
@@ -2,9 +2,12 @@ package mvc.user.controller;
 
 import java.util.List;
 
+import javax.validation.Valid;
+
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
+import org.springframework.validation.BindingResult;
 import org.springframework.web.bind.annotation.DeleteMapping;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.ModelAttribute;
@@ -12,7 +15,6 @@ import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.PutMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.ResponseBody;
 
 import mvc.user.dao.BaseDataDao;
 import mvc.user.model.dto.UserDto;
@@ -24,17 +26,14 @@ import mvc.user.model.po.User;
 import mvc.user.service.UserService;
 
 /**
- * 定義 URI 服務 
- * ------------------------------------------------------------------
+ * 定義 URI 服務 ------------------------------------------------------------------
  * Method | URI | Description
- * ------------------------------------------------------------------ 
- * GET    | /user   | 取得所有使用者資料並重導到/WEB-INF/view/user/user.jsp頁面
- * GET    | /user/1 | 根據 userId 取得單筆使用者資料 
- * POST   | /user   | 新增使用者資料, 會自動夾帶 User 物件資料上來 
- * PUT    | /user/1 | 修改指定 userId 的使用者資料, 會自動夾帶要修改的 User 物件資料上來
- * DELETE | /user/1 | 刪除指定 userId 的使用者紀錄
- * ------------------------------------------------------------------ 
- * URL 範例: GET http://localhost:8080/SpringMVC/mvc/user
+ * ------------------------------------------------------------------ GET |
+ * /user | 取得所有使用者資料並重導到/WEB-INF/view/user/user.jsp頁面 GET | /user/1 | 根據 userId
+ * 取得單筆使用者資料 POST | /user | 新增使用者資料, 會自動夾帶 User 物件資料上來 PUT | /user/1 | 修改指定
+ * userId 的使用者資料, 會自動夾帶要修改的 User 物件資料上來 DELETE | /user/1 | 刪除指定 userId 的使用者紀錄
+ * ------------------------------------------------------------------ URL 範例:
+ * GET http://localhost:8080/SpringMVC/mvc/user
  */
 
 @Controller
@@ -43,21 +42,21 @@ public class UserController {
 
 	@Autowired
 	private UserService userService;
-	
+
 	@Autowired
 	private BaseDataDao baseDataDao;
-	
+
 	@GetMapping
 	// model: 欲將給 jsp 的資料要放在 model 容器中
 	public String queryAllUsers(@ModelAttribute User user, Model model) {
 		// 基本要傳給 user.jsp 的資訊
 		addBasicModel(model);
 		// 預設年齡
-		user.setAge(18); 
+		user.setAge(18);
 		// 完整 jsp(view) 路徑 = "/WEB-INF/view/user/user.jsp";
 		// 因為在 springmvc-servlet.xml
 		// 已經定義: prefix = "/WEB-INF/view/"
-		//        suffix = ".jsp"
+		// suffix = ".jsp"
 		// 所以只要寫成 "user/user"
 		return "user/user";
 
@@ -76,24 +75,43 @@ public class UserController {
 	}
 
 	@PostMapping("/")
-	public String createUser(User user) {
+	public String createUser(@Valid User user, BindingResult result, Model model) {
+		// 判斷驗證是否通過?
+		if (result.hasErrors()) {
+			addBasicModel(model);
+			// 有錯誤的user 資料也一併帶入給表單使用
+			// 內涵錯誤的原因
+			model.addAttribute("user", user);
+			return "/user/user";
+		}
 		Boolean success = userService.addUser(user);
-		return "redirect:/mvc/user ";
+		return "redirect:/mvc/user";
 	}
 
 	@PutMapping("/{userId}")
-	public String updateUser(@PathVariable("userId") Integer userId, User user) {
-		System.out.println(user);
+	public String updateUser(@PathVariable("userId") Integer userId, @Valid User user, BindingResult result,
+			Model model) {
+		// 判斷驗證是否通過?
+		if (result.hasErrors()) {
+			addBasicModel(model);
+			// 有錯誤的user 資料也一併帶入給表單使用
+			// 內涵錯誤的原因
+			model.addAttribute("user", user);
+			// 要傳PUT
+			model.addAttribute("_method", "PUT");
+			return "/user/user";
+		}
+
 		Boolean success = userService.updateUser(userId, user);
 		return "redirect:/mvc/user";
 	}
 
-	@DeleteMapping("/{userId}")	
+	@DeleteMapping("/{userId}")
 	public String deleteUser(@PathVariable("userId") Integer userId) {
 		Boolean success = userService.deleteUser(userId);
 		return "redirect:/mvc/user";
 	}
-	
+
 	// 基本要傳給首頁 user.jsp 的資料
 	private void addBasicModel(Model model) {
 		// 基本要傳給 jsp 的資訊
@@ -103,7 +121,7 @@ public class UserController {
 		List<Interest> interests = baseDataDao.findAllInterests(); // 所有興趣
 		List<Statistics> genderStatistics = userService.queryStatistics("Gender"); // 統計性別
 		List<Statistics> educationStatistics = userService.queryStatistics("Education"); // 統計學歷
-		
+
 		model.addAttribute("userDtos", userDtos);
 		model.addAttribute("educations", educations);
 		model.addAttribute("genders", genders);
