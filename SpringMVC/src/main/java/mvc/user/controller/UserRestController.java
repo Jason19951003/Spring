923665cdaefd19406044a752521a8a9a2c3953a9
Jason19951003 <Jason19951003@gmail.com>
2024-05-14 16:17:54 +0800

    刪除多餘的程式碼

--- SpringMVC/src/main/java/mvc/user/controller/UserRestController.java
+++ SpringMVC/src/main/java/mvc/user/controller/UserRestController.java
@@ -2,11 +2,7 @@ package mvc.user.controller;
 
 import java.util.List;
 
-import javax.validation.Valid;
-
 import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.ui.Model;
-import org.springframework.validation.BindingResult;
 import org.springframework.web.bind.annotation.DeleteMapping;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
