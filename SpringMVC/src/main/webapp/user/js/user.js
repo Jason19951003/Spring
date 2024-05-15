2335735c7e3c34eea9dfc1ddfaf6af2f349a8140
xiang1003 <xiang1003@localhost>
2024-05-15 15:37:22 +0800

    完成刪除

--- SpringMVC/src/main/webapp/user/js/user.js
+++ SpringMVC/src/main/webapp/user/js/user.js
@@ -18,12 +18,12 @@ const fetchAndRenderData = async(url, containerId, renderFn) => {
 		const {state, message, data} = await response.json(); // 等待回應本文內容
 		//console.log(state, message, data);
 		//console.log(renderFn(data[0]));
-		if(Arrays.isArray(data)) {
+		/*if(Arrays.isArray(data)) {
 			$(containerId).innerHTML = data.map(renderFn).join('');
 		} else {
 			$(containerId).innerHTML = renderFn(data);
-		}
-		
+		}*/
+		$(containerId).innerHTML = Array.isArray(data) ? data.map(renderFn).join('') : renderFn(data);
 	} catch(e) {
 		console.error(e);
 	}
@@ -34,8 +34,12 @@ const renderUser = ({id, name, gender, age, birth, education,interestNames, resu
 	<tr>
 		<td>${id}</td><td>${name}</td><td>${gender.name}</td><td>${age}</td><td>${birth}</td>
 		<td>${education.name}</td><td>${interestNames}</td><td>${resume}</td>
-		<td>修改</td>
-		<td>刪除</td>
+		<td title="修改">
+			<span class="button-update pure-button update-user-button" data-id=${id}>修改</span
+		</td>
+		<td title="刪除">
+			<span class="button-delete pure-button delete-user-button" data-id=${id}>刪除</span>
+		</td>
 	</tr>
 `;
 
@@ -46,5 +50,39 @@ document.addEventListener("DOMContentLoaded", async() => {
 	await loadHTML('/user/user-list.html', 'user-list-container');
 	
 	// 資料渲染(fetch取資料+渲染)
-	fetchAndRenderData('/mvc/rest/user', 'user-list-body', renderUser);	
+	fetchAndRenderData('/mvc/rest/user', 'user-list-body', renderUser);
+	// 監聽UserList 是否有被點擊
+	$('user-list-table').addEventListener("click", async(event) => {
+		// 處理事件
+		await handleEvent(event, 'update-user-button', handleUpdateUser);
+		await handleEvent(event, 'delete-user-button', handleDeleteUser);
+	});
 });
+
+const handleUpdateUser = (id) => { console.log('按下修改' + id) };
+const handleDeleteUser = async(id) => {	
+	const result = await Swal.fire({
+		title: '確定要刪除嗎?',
+		text: '刪除後無法恢復',
+		icon: 'warning',
+		showCancelButton: true,
+		confirmButtonText: '是的, 刪除它',
+		cancelButtonText: '取消'
+	});
+	
+	if(result.isConfirmed) {
+		const fullurl = 'http://localhost:8080/SpringMVC/mvc/rest/user/' + id;	
+		const response = await fetch(fullurl, {method : 'DELETE'}); // 等待fetch 請求完成
+		const {state, message, data} = await response.json(); // 等待回應本文內容
+		console.log({state, message, data});
+		fetchAndRenderData('/mvc/rest/user', 'user-list-body', renderUser);
+	}
+};
+
+const handleEvent = async(event, className, callback) => {
+	if(!event.target.classList.contains(className)) {
+		return;
+	}
+	const id = event.target.getAttribute('data-id');
+	callback(id);
+};
