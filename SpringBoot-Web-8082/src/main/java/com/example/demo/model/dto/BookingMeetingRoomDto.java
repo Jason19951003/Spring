8de839b0e1b123536f587cbad681f4c544bf8dcf
xiang1003 <xiang1003@localhost>
2024-05-20 12:24:29 +0800

    SpringBoot-web-8082

---
+++ SpringBoot-Web-8082/src/main/java/com/example/demo/model/dto/BookingMeetingRoomDto.java
@@ -0,0 +1,25 @@
+package com.example.demo.model.dto;
+
+import java.sql.Timestamp;
+
+import com.example.demo.model.po.MeetingRoom;
+import com.example.demo.model.po.User;
+
+import lombok.AllArgsConstructor;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+@Data
+@NoArgsConstructor
+@AllArgsConstructor
+public class BookingMeetingRoomDto {
+	private Integer bookingId; 
+	private Integer roomId;
+    private Integer userId;     
+    private String bookingDate;
+    private Timestamp timestamp;
+    
+    // 關聯欄位(多對一)
+    private MeetingRoom  meetingRoom;
+    private User user;
+}
