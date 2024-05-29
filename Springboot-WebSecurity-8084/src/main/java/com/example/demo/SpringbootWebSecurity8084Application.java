cb9e12fb0549a95ea0936fa880bfb2958b56e942
Jason19951003 <Jason19951003@gmail.com>
2024-05-29 11:54:46 +0800

    修改filter

--- Springboot-WebSecurity-8084/src/main/java/com/example/demo/SpringbootWebSecurity8084Application.java
+++ Springboot-WebSecurity-8084/src/main/java/com/example/demo/SpringbootWebSecurity8084Application.java
@@ -2,25 +2,11 @@ package com.example.demo;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
-import org.springframework.boot.web.servlet.FilterRegistrationBean;
 import org.springframework.boot.web.servlet.ServletComponentScan;
-import org.springframework.context.annotation.Bean;
-import org.springframework.context.annotation.Configuration;
-
-import com.example.demo.filter.HstsFilter;
 
 @SpringBootApplication
 @ServletComponentScan // 支援傳統JavaWeb 技術
-@Configuration
 public class SpringbootWebSecurity8084Application {
-
-	@Bean
-    public FilterRegistrationBean<HstsFilter> hstsFilterRegistrationBean() {
-        FilterRegistrationBean<HstsFilter> registrationBean = new FilterRegistrationBean<>();
-        registrationBean.setFilter(new HstsFilter());
-        registrationBean.addUrlPatterns("/*");
-        return registrationBean;
-    }
 
 	public static void main(String[] args) {
 		SpringApplication.run(SpringbootWebSecurity8084Application.class, args);
--- Springboot-WebSecurity-8084/src/main/java/com/example/demo/filter/HstsFilter.java
+++ Springboot-WebSecurity-8084/src/main/java/com/example/demo/filter/HstsFilter.java
@@ -7,8 +7,10 @@ import jakarta.servlet.FilterChain;
 import jakarta.servlet.ServletException;
 import jakarta.servlet.ServletRequest;
 import jakarta.servlet.ServletResponse;
+import jakarta.servlet.annotation.WebFilter;
 import jakarta.servlet.http.HttpServletResponse;
 
+@WebFilter("/*")
 public class HstsFilter implements Filter {
 
     @Override
