0b8d738da739cbfa50bb4933c2262641a973bcbb
Jason19951003 <Jason19951003@gmail.com>
2024-05-29 11:28:02 +0800

    將標頭資訊傳遞給瀏覽器, 瀏覽器就會根據此標頭進行保護

---
+++ Springboot-WebSecurity-8084/src/main/java/com/example/demo/filter/HstsFilter.java
@@ -0,0 +1,28 @@
+package com.example.demo.filter;
+
+import java.io.IOException;
+
+import jakarta.servlet.Filter;
+import jakarta.servlet.FilterChain;
+import jakarta.servlet.ServletException;
+import jakarta.servlet.ServletRequest;
+import jakarta.servlet.ServletResponse;
+import jakarta.servlet.http.HttpServletResponse;
+
+public class HstsFilter implements Filter {
+
+    @Override
+    public void doFilter(ServletRequest request, ServletResponse response, FilterChain chain)
+            throws IOException, ServletException {
+        if (response instanceof HttpServletResponse) {
+            // 將標頭資訊傳遞給瀏覽器, 瀏覽器就會根據此標頭進行保護
+        	HttpServletResponse httpServletResponse = (HttpServletResponse) response;
+            httpServletResponse.setHeader("Strict-Transport-Security", "max-age=31536000; includeSubDomains");
+            httpServletResponse.setHeader("X-Frame-Options", "DENY");
+            httpServletResponse.setHeader("X-Content-Type-Options", "nosniff");
+            httpServletResponse.setHeader("X-XSS-Protection", "1; mode=block");
+        }
+        chain.doFilter(request, response);
+    }
+
+}
