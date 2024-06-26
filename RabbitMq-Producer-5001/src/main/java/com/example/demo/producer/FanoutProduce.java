925d1a56cfc19dc7554c1e631377878066787be6
Jason19951003 <Jason19951003@gmail.com>
2024-06-26 14:21:33 +0800

    rabbitMQ

--- RabbitMq-Producer-5001/src/main/java/com/example/demo/producer/FanoutProduce.java
+++ RabbitMq-Producer-5001/src/main/java/com/example/demo/producer/FanoutProduce.java
@@ -2,6 +2,9 @@ package com.example.demo.producer;
 
 import java.util.Date;
 
+import org.springframework.amqp.core.Message;
+import org.springframework.amqp.core.MessageDeliveryMode;
+import org.springframework.amqp.core.MessageProperties;
 import org.springframework.amqp.rabbit.core.RabbitTemplate;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.web.bind.annotation.GetMapping;
@@ -15,8 +18,16 @@ public class FanoutProduce {
 	
 	@GetMapping("/sendFanout")
 	public String sendFanout() {
+		// 非持久化訊息
 		String data = "Hello Fanout Exchange: " + new Date();
-		rabbitTemplate.convertAndSend("fanout-exchange", "", data);
+		//rabbitTemplate.convertAndSend("fanout-exchange", "", data);
+		
+
+		// 持久化訊息
+		MessageProperties messageProperties = new MessageProperties();
+		messageProperties.setDeliveryMode(MessageDeliveryMode.PERSISTENT);
+		Message message = new Message(data.getBytes(), messageProperties);
+		rabbitTemplate.send("fanout-exchange", "", message);
 		return "Message: [ " + data + " ] send to Fanout Exchange]";
 	}
 }
