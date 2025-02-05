0089ab56dc4a0da3f7fce6dab8983b9bc4c914b5
游智翔 <zhixiang@cht.com.tw>
2025-02-05 15:05:19 +0800

    使用scheduled 每秒發送訊息給前端

---
+++ stomp-websocket/src/main/java/com/jason/stompwebsocket/scheduled/MessageScheduled.java
@@ -0,0 +1,19 @@
+package com.jason.stompwebsocket.scheduled;
+
+import com.jason.stompwebsocket.model.Greeting;
+import lombok.RequiredArgsConstructor;
+import org.springframework.messaging.simp.SimpMessagingTemplate;
+import org.springframework.scheduling.annotation.Scheduled;
+import org.springframework.stereotype.Component;
+
+@Component
+@RequiredArgsConstructor
+public class MessageScheduled {
+
+    private final SimpMessagingTemplate messagingTemplate;
+
+    @Scheduled(fixedRate = 1000)
+    public void sendMessage() {
+        messagingTemplate.convertAndSend("/topic/greetings", new Greeting("Hello I'm Jason"));
+    }
+}
